9584ac4d0982e248bd84e44538ee680a79468ed5
Fagan <mustafafagan@gmail.com>
2024-06-12 10:03:33 +0700

    Nerfing

--- PlantsVsZombies/src/Main/LevelData.java
+++ PlantsVsZombies/src/Main/LevelData.java
@@ -5,10 +5,6 @@ package Main;/*
  */
 import java.io.*;
 import java.util.logging.Logger;
-/**
- *
- * @author Vaibhav Singh Sikarwar
- */
 public class LevelData {
     static String Lvl = "1";
    static String [][] Level = {{"Object.Enemies.NormalZombie"},{"Object.Enemies.NormalZombie","Object.Enemies.ConeHeadZombie"}};
